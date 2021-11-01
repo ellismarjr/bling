60219df29917100c2ca2ddcac3d1cccfee405f21
Junior de Oliveira <ellismarjr@hotmail.com>
2021-11-01 11:21:20 -0300

    chore: add styles

--- github_mobile/App.tsx
+++ github_mobile/App.tsx
@@ -1,11 +1,61 @@
 import React from 'react';
 
-import {Text, View} from 'react-native';
+import {Image, Text, TextInput, TouchableOpacity, View} from 'react-native';
+
+import Feather from 'react-native-vector-icons/Feather';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+import {styles} from './styles';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Dados do repositório</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Pesquisar pelo repositório"
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardAppearance="dark"
+        />
+        <TouchableOpacity activeOpacity={0.8} style={styles.button}>
+          <Text>Pesquisar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.contentItem}>
+          <Text style={styles.contentTitle}>ID:</Text>
+          <Text style={styles.contentItemText}>23088740</Text>
+        </View>
+        <View style={styles.contentItem}>
+          <Text style={styles.contentTitle}>Name:</Text>
+          <Text style={styles.contentItemText}>23088740</Text>
+        </View>
+        <View style={styles.contentItem}>
+          <Text style={styles.contentTitle}>Fullname:</Text>
+          <Text style={styles.contentItemText}>23088740</Text>
+        </View>
+        <View style={styles.contentItem}>
+          <Text style={styles.contentTitle}>Owner:</Text>
+          <Image
+            style={styles.image}
+            source={{uri: 'https://github.com/ellismarjr.png'}}
+          />
+          <Text style={styles.contentItemText}>23088740</Text>
+        </View>
+        <View style={styles.footer}>
+          <View style={styles.footerItem}>
+            <Text style={styles.footerTitle}>23088740</Text>
+            <Feather name="star" size={16} color="#333" />
+          </View>
+          <View style={styles.footerItem}>
+            <Text style={styles.footerTitle}>23088740</Text>
+            <Entypo name="flow-branch" size={16} color="#333" />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
---
+++ github_mobile/styles.ts
@@ -0,0 +1,86 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingBottom: 10,
+    color: '#121214',
+    textAlign: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#e4e4e4',
+    flex: 1,
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+    height: 50,
+    paddingHorizontal: 10,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+    width: 100,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  content: {
+    borderWidth: 1,
+    borderColor: '#c3c3c3',
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  contentItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#c3c3c3',
+    padding: 10,
+  },
+  image: {
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+    marginLeft: 5,
+  },
+
+  contentTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#121214',
+  },
+  contentItemText: {
+    marginLeft: 10,
+    color: '#333',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  footerItem: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  footerTitle: {
+    fontWeight: 'bold',
+    color: '#121214',
+    marginRight: 10,
+  },
+});
